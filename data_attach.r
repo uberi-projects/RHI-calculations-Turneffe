e28035a89bd9ccc2af382d25765bd59771581c94
sharp-lilac <jessicacboles@gmail.com>
2026-02-06 14:05:22 -0600

    Read in data to dataframes

--- data_attach.r
+++ data_attach.r
@@ -12,3 +12,18 @@ if (dir.exists("data_deposit") && length(list.files("data_deposit")) > 0) {
     file.copy(unlist(downloaded_files), "data_deposit", overwrite = TRUE)
     message("Files copied to data_deposit/")
 }
+
+## Attach data --------------------------------------
+na_strings <- c("NA", "", "MISSING", "UNKNOWN")
+df_benthic_inverts <- read.csv("data_deposit/Master_Benthic_Inverts_2010-2025.csv", na.strings = na_strings)
+df_benthic_pim <- read.csv("data_deposit/Master_Benthic_PIM_2010-2025.csv", na.strings = na_strings)
+df_benthic_recruits <- read.csv("data_deposit/Master_Benthic_Recruit_2023-2025.csv", na.strings = na_strings)
+df_coral <- read.csv("data_deposit/Master_Coral_Community_2010-2025.csv", na.strings = na_strings)
+df_fish <- read.csv("data_deposit/Master_Fish_Survey_2010-2025.csv", na.strings = na_strings)
+ref_collectors <- read.csv("data_deposit/Ref_Collectors_Turneffe.csv", na.strings = na_strings)
+ref_diseases <- read.csv("data_deposit/Ref_Diseases_Coral.csv", na.strings = na_strings)
+ref_fish_sizes <- read.csv("data_deposit/Ref_Fish_Sizes.csv", na.strings = na_strings)
+ref_fish_species <- read.csv("data_deposit/Ref_Fish_Species.csv", na.strings = na_strings)
+ref_organisms_benthic <- read.csv("data_deposit/Ref_Organisms_Benthic.csv", na.strings = na_strings)
+ref_sites <- read.csv("data_deposit/Ref_Sites_Turneffe.csv", na.strings = na_strings)
+ref_substrates <- read.csv("data_deposit/Ref_Substrates_Benthic.csv", na.strings = na_strings)
